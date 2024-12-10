2e8975de89bddafa2a62a12aa767a8e09dd562fa
Vedikakoravi <159905384+Vedikakoravi@users.noreply.github.com>
2024-12-10 15:12:20 +0530

    Add files via upload
    Input: Take marks obtained (out of 100) in each subject.
    Calculate Total Marks: Sum up the marks obtained in all subjects.
    Calculate Average Percentage: Divide the total marks by the total number of subjects to get the
    average percentage.
    Grade Calculation: Assign grades based on the average percentage achieved.
    Display Results: Show the total marks, average percentage, and the corresponding grade to the user

---
+++ Task2.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Task2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int numberOfSubjects = 5; 
+        int[] marks = new int[numberOfSubjects];
+        int totalMarks = 0;
+        double averagePercentage;
+        String grade;
+
+        for (int i = 0; i < numberOfSubjects; i++) {
+            System.out.print("Enter marks for subject " + (i + 1) + ": ");
+            marks[i] = scanner.nextInt();
+            totalMarks += marks[i];
+        }
+
+        averagePercentage = (double) totalMarks / numberOfSubjects;
+
+        if (averagePercentage >= 90) {
+            grade = "A+";
+        } else if (averagePercentage >= 80) {
+            grade = "A";
+        } else if (averagePercentage >= 70) {
+            grade = "B";
+        } else if (averagePercentage >= 60) {
+            grade = "C";
+        } else if (averagePercentage >= 50) {
+            grade = "D";
+        } else {
+            grade = "F";
+        }
+        System.out.println("Total Marks: " + totalMarks);
+        System.out.println("Average Percentage: " + averagePercentage + "%");
+        System.out.println("Grade: " + grade);
+
+        scanner.close();
+    }
+}
